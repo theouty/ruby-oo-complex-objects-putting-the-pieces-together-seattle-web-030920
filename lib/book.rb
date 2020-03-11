d45aa764e1a881ae9ffc17399c100fc56aff1c9e
Aman Michael <aman.david.michael@gmail.com>
2020-03-11 21:05:04 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
-
-
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  
+  def initialize(title)
+    @title = title
+  end 
+  def title 
+    @title
+  end 
+  
+  def turn_page
+    @page_count += 1;
+end 
+end
